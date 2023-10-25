5c144f55bed388e2f2155e3289dd7e6039fe2d4e
aki32585 <41956614+aki32585@users.noreply.github.com>
2023-10-25 20:40:17 +0900

    コメントアウト修正

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ async def on_message(message):
 
     urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
     
-    # "x.com"を含むURLが見つかった場合
+    #"x.com"を含むURLが見つかった場合
     for url in urls:
         if "https://x.com" in url:
             # URLを改変して新しいメッセージを生成
             modified_url = url.replace("x.com", "fxtwitter.com")
-            
-            # ここに作動させたい処理を追加
+            #リプライで送る(メンションなし)
             await message.reply(f"{modified_url}")
+    #"twitter.com"を含むURLが見つかった場合
     for url in urls:
         if "https://twitter.com" in url:
             # URLを改変して新しいメッセージを生成
             modified_url = url.replace("twitter.com", "fxtwitter.com")
             
-            # ここに作動させたい処理を追加
+            #リプライで送る(メンションなし)
             await message.reply(f"{modified_url}", mention_author=False)
 
 client.run(TOKEN)
