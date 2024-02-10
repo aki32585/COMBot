91020b5926615f565654d972c01120a2ff450df6
aki32585 <41956614+aki32585@users.noreply.github.com>
2024-02-10 23:08:44 +0900

    twitter展開廃止

--- main.py
+++ main.py
@@ -19,22 +19,22 @@ async def on_message(message):
         return
     await dispand(message)
 
-    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
+    # urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
 
-    # "x.com"を含むURLが見つかった場合
-    for url in urls:
-        if "https://x.com" in url:
-            # URLを改変して新しいメッセージを生成
-            modified_url = url.replace("x.com", "fxtwitter.com")
-            # リプライで送る(メンションなし)
-            await message.channel.send(f"{modified_url}", mention_author=False)
-    # "twitter.com"を含むURLが見つかった場合
-    for url in urls:
-        if "https://twitter.com" in url:
-            # URLを改変して新しいメッセージを生成
-            modified_url = url.replace("twitter.com", "fxtwitter.com")
+    # # "x.com"を含むURLが見つかった場合
+    # for url in urls:
+    #     if "https://x.com" in url:
+    #         # URLを改変して新しいメッセージを生成
+    #         modified_url = url.replace("x.com", "fxtwitter.com")
+    #         # リプライで送る(メンションなし)
+    #         await message.channel.send(f"{modified_url}", mention_author=False)
+    # # "twitter.com"を含むURLが見つかった場合
+    # for url in urls:
+    #     if "https://twitter.com" in url:
+    #         # URLを改変して新しいメッセージを生成
+    #         modified_url = url.replace("twitter.com", "fxtwitter.com")
 
-            # リプライで送る(メンションなし)
-            await message.channel.send(f"{modified_url}", mention_author=False)
+    #         # リプライで送る(メンションなし)
+    #         await message.channel.send(f"{modified_url}", mention_author=False)
 
 client.run(TOKEN)
